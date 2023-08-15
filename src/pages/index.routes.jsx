31f18b2c8477ed4c34cba54522ebef4360599ddf
Brandon Quintero <developerbvth@gmail.com>
2023-08-15 01:15:08 -0500

    Empezando a hacer ofertas

--- src/pages/index.routes.jsx
+++ src/pages/index.routes.jsx
@@ -2,6 +2,7 @@ import { HeaderAndAside } from "./templates/index.template";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Home.routes";
 import { Managers, Property, Owners } from "./Property.pages/";
+import { Offers } from "./Offers";
 export function Index() {
   return (
     <>
@@ -12,6 +13,7 @@ export function Index() {
           <Route path="/inmueble" element={<Property />} />
           <Route path="/propietario" element={<Owners />} />
           <Route path="/encargado" element={<Managers />} />
+          <Route path="/oferta" element={<Offers />} />
         </Routes>
       </div>
       {/* <Footer /> */}
