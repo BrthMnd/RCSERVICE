11b3bc16c4c7025be410308bbec2650c57de2452
Brandon Quintero <developerbvth@gmail.com>
2023-09-12 14:33:59 -0500

    adad

--- src/Utils/ActionsTable.jsx
+++ src/Utils/ActionsTable.jsx
@@ -1,40 +1,22 @@
-import { useDispatch } from "react-redux";
-
-export function AddActions() {
-  console.log("yes");
-  const dispatch = useDispatch();
-  const handleDispatch = () => {
-    // Aquí creas una acción utilizando tu acción de Redux y luego la envías mediante dispatch
-    dispatch(ChageValue(true)); // Suponiendo que tienes una acción llamada ChageValue
-  };
-  handleDispatch();
-  return;
-}
-export function EditActions(table) {
-  alert("editando...");
-}
-export function DeleteActions(table) {
-  alert("Eliminando...");
-}
-
-export const ButtonAction = (value, tableMeta, list) => {
+/* eslint-disable react/prop-types */
+import { EditButton } from "./edit_button";
+import { AlertDelete } from "../assets/js/Alerts";
+export const ButtonAction = ({ tableMeta, list, url, title }) => {
   const rowData = list[tableMeta.rowIndex];
+
   return (
-    <div className="buttons__actions">
-      <button
-        type="button"
-        className="btn btn-warning"
-        onClick={() => EditActions(rowData)}
-      >
-        <i className="fas fa-edit"></i>
-      </button>
-      <button
-        type="button"
-        className="btn btn-danger"
-        onClick={() => DeleteActions(rowData)}
-      >
-        <i className="fas fa-trash"></i>
-      </button>
-    </div>
+    <>
+      <div className="buttons__actions">
+        <EditButton title={title} table={rowData} URL={url} />
+        <button
+          type="button"
+          className="btn btn-danger"
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          onClick={() => AlertDelete(url, rowData)}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
+    </>
   );
 };
