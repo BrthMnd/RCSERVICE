6a632f7d4856c5fa9ec5524ed567ddfcf94e2d90
Cristian Pardo <cristianpardo044@gmail.com>
2023-09-14 15:41:05 -0500

    cris3

--- src/components/Modals/Forms/services/service.categority.modal.jsx
+++ src/components/Modals/Forms/services/service.categority.modal.jsx
@@ -1,19 +1,53 @@
-export function CategoriaServicioModal(props) {
+import { useDispatch, useSelector } from "react-redux";
+import { ApiPut, useApiPost } from "../../../../hooks/useApi";
+import { useEffect, useState } from "react";
+import { changeDataVoid } from "../../../../features/modal/moda.slice";
+
+const url = "https://rcservice.onrender.com/api/proveedores/Categoria";
+
+export function CategoriaServicioModal() {
+  const [empty, setEmpty] = useState(true);
+  const dispatch = useDispatch();
+
+  let data = useSelector((state) => state.modal.data);
+
+  const HandlePost = (e) => {
+    e.preventDefault();
+
+    const resultado = {
+      Nombre_Categoria: e.target.NombreCategoria.value,
+      Descripcion: e.target.DescripcionCategoria.value,
+    };
+
+    // dispatch(changeDataVoid());
+    useApiPost(url, resultado);
+    dispatch(changeDataVoid());
+  };
+
+  const HandlePut = (e) => {
+    e.preventDefault();
+
+    const resultado = {
+      id: data.id,
+      Nombre_Categoria: e.target.NombreCategoria.value,
+      Descripcion: e.target.DescripcionCategoria.value,
+    };
+    ApiPut(url, resultado);
+    dispatch(changeDataVoid());
+  };
+  useEffect(() => {
+    console.log("effect");
+    if (Object.keys(data).length !== 0) {
+      setEmpty(false);
+    } else {
+      setEmpty(true);
+    }
+  }, [data]);
+
   return (
     <>
-      <form className="row g-3">
+      <form className="row g-3" onSubmit={empty ? HandlePost : HandlePut}>
         <div className="col-md-6">
-          <div className="mb-3">
-            <label htmlFor="inputIdCategoria" className="form-label">
-              ID de Categoría
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputIdCategoria"
-              placeholder="Ingrese el ID de la categoría"
-            />
-          </div>
           <div className="mb-3">
             <label htmlFor="inputNombreCategoria" className="form-label">
               Nombre de la Categoría
@@ -22,7 +56,8 @@ export function CategoriaServicioModal(props) {
               type="text"
               className="form-control"
               id="inputNombreCategoria"
-              placeholder="Ingrese el nombre de la categoría"
+              name="NombreCategoria"
+              defaultValue={empty ? "" : data.Nombre_Categoria}
             />
           </div>
         </div>
@@ -36,6 +71,8 @@ export function CategoriaServicioModal(props) {
               id="inputDescripcionCategoria"
               rows="4"
               placeholder="Ingrese una descripción de la categoría"
+              name="DescripcionCategoria"
+              defaultValue={empty ? "" : data.Descripcion}
             ></textarea>
           </div>
         </div>
