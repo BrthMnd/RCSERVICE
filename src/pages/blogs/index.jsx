c38fc769b5b9ce57704c8381dd4867ec184ad3fb
Brandon Quintero <developerbvth@gmail.com>
2023-11-01 15:51:06 -0500

    Blog*

---
+++ src/pages/blogs/index.jsx
@@ -0,0 +1,11 @@
+import { OffersHelps } from "./templates/offers.help";
+
+export default function Blog() {
+  return (
+    <>
+      <section id="Blog">
+        <OffersHelps />
+      </section>
+    </>
+  );
+}
