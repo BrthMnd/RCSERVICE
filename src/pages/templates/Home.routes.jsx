99152092b4368d06ec01db18044ecb9457fd0f60
Cristian Pardo <cristianpardo044@gmail.com>
2023-11-05 23:42:04 -0500

    Pruebas Dashboard

--- src/pages/templates/Home.routes.jsx
+++ src/pages/templates/Home.routes.jsx
@@ -1,18 +1,9 @@
+import { Dashboard } from "../Dashboard/Graficas.routes";
+
 export function Home() {
   return (
     <div className="sections">
-      <h1>Hola</h1>
+      <Dashboard />
     </div>
   );
-}
-
-// import {ApiGet} from "../../hooks/useApi"
-// import {Line} from "react-chartjs-2"
-// import {Chart as ChartJS, CategoryScale} from "chart.js";
-
-
-export function Dashboard(){
-  return (
-    <h1>Esto va a ser el dashboard</h1>
-  )
 }
