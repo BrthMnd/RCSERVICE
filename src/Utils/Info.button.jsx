118674c1adf538c2facd1563aff29243b095aea8
Dufainer <dufainer@gmail.com>
2023-11-23 00:41:16 -0500

    algo

---
+++ src/Utils/Info.button.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import {
+  changeData,
+  changeModal,
+  changeUrl,
+} from "../features/modal/moda.slice";
+
+export function InfoButton({ title, URL, table }) {
+  const dispatch = useDispatch();
+  const handleClickEdit = () => {
+    dispatch(changeData(table));
+    dispatch(changeModal(title));
+    dispatch(changeUrl(URL));
+  };
+  return (
+    <>
+      <span
+        data-bs-toggle="tooltip"
+        data-bs-placement="bottom"
+        title="Mas información"
+      >
+        <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#ModalFather"
+          className="btn btn-primary"
+          onClick={handleClickEdit}
+        >
+          <i className="fas fa-info"></i>
+        </button>
+      </span>
+    </>
+  );
+}
