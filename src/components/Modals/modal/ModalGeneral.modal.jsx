9f8f20c934c02c187a2ff700cd7197cd3e961596
Brandon Quintero <developerbvth@gmail.com>
2023-11-22 21:28:09 -0500

    up for cris

---
+++ src/components/Modals/modal/ModalGeneral.modal.jsx
@@ -0,0 +1,46 @@
+import { useDispatch } from "react-redux";
+import {
+  changeDataVoid,
+  changeModalVoid,
+} from "../../../features/modal/moda.slice";
+
+export function ModalGeneral({ titleProperty, Titulo, children }) {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(changeDataVoid());
+    dispatch(changeModalVoid());
+  };
+  return (
+    <div
+      className="modal fade"
+      id="ModalFather"
+      tabIndex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      data-bs-backdrop="static"
+    >
+      <div
+        className={`modal-dialog modal-dialog-centered ${
+          // Titulo == "Candidatos de Oferta" ? "modal-xl" : "modal-lg"
+          titleProperty()
+        }`}
+      >
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              {Titulo}
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={handleClick}
+            ></button>
+          </div>
+          <div className="modal-body">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
