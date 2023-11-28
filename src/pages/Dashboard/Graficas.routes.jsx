f7c117e121bbb1bc25b046d71a6676d0e946a37c
jeni bejarano <jenim@JENI0708.(none)>
2023-11-28 02:48:58 -0500

    dashboard

--- src/pages/Dashboard/Graficas.routes.jsx
+++ src/pages/Dashboard/Graficas.routes.jsx
@@ -4,34 +4,37 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Graficas } from "./Categorias";
 import { GraficasInmueble } from "./Inmuebles";
 import { GraficaServicios } from "./Servicios";
-import {IconLoading} from "../../Utils/IconsLoading"
+import { IconLoading } from "../../Utils/IconsLoading";
 ChartJS.register(ArcElement, Tooltip, Legend);
-
+const styles = {
+  width: "100%",
+  height:"100vh",
+  overflow: "auto",
+  display: "grid",
+};
 export function Dashboard() {
   const [data, loadingData, errorData] = ApiGet(
     "https://rcservice.onrender.com/api/dashboard"
   );
-  if (!loadingData && !errorData ){
-    console.log(data)
+  if (!loadingData && !errorData) {
+    console.log(data);
   }
 
   return (
-    loadingData && (
-      <IconLoading />
-    ),
+    loadingData && <IconLoading />,
     errorData && (
-      <h1>Error <span>{errorData.message}</span></h1>
-
+      <h1>
+        Error <span>{errorData.message}</span>
+      </h1>
     ),
     !loadingData && !errorData && (
       <>
-      <div class="row row-cols-3 justify-content-center text-center">
-        <Graficas data={data.categorias} />
-        <GraficasInmueble data={data.inmuebles}/> 
-        <GraficaServicios data={data.servicios} />  
-      </div> 
+        <div style={styles}>
+          <Graficas data={data.categorias} />
+          <GraficasInmueble data={data.inmuebles} />
+          <GraficaServicios data={data.servicios} />
+        </div>
       </>
-
-    ) 
+    )
   );
 }
--- src/pages/templates/Home.routes.jsx
+++ src/pages/templates/Home.routes.jsx
@@ -1,19 +1,7 @@
-import { ApiGet } from "../../hooks/useApi";
-import {Graficas} from "../Dashboard/Categorias";
 import { Dashboard } from "../Dashboard/Graficas.routes";
-import { GraficasInmueble } from "../Dashboard/Inmuebles";
 
 export function Home() {
-  const cardStyle = {
-    top: "20px",
-    left: "20px",
-    width: "500px",
-    padding: "30px",
-    borderRadius: "10px",
-      // border:"1px solid black",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    
-  };
+  
    
   return (
     <Dashboard />
