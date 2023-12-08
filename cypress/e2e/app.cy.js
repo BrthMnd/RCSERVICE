dd4bc98c850541ae19da0117ce6668297d28c807
Cristian Pardo <cristianpardo044@gmail.com>
2023-12-08 10:54:54 -0500

    cypress

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,25 +1,16 @@
-describe('index', () => {
-    beforeEach(()=>{
-        cy.visit("http://localhost:5173/")
-    })
-    it("Verificar que el acordeon de lista se Abre", ()=>{
-
-        cy.get('.nav-pills > :nth-child(3) > :nth-child(1)').click({force: true}).as("ClickOffers")
-        cy.get("#ul-test").should("have.css","display","block")
-        cy.get('@ClickOffers').click({force: true})
-        cy.get("#ul-test").should("have.css","display","none")
-        // cy.debug()
-        // cy.get('@ClickOffers').click({force: true})
-        // cy.get("#ul-test").should("have.css","display","none")
-
-
-        // cy.contains('.nav-treeview', 'Texto esperado en el contenido del acordeón').should('be.visible')
-        // cy.get('.nav-pills > :nth-child(3) > :nth-child(1) .nav-treeview',).should('be.visible')
-    })
-    it.only("Visitar ofertas",()=>{
-        cy.visit("http://localhost:5173/servicios/servicio")
-        
-        
-    })
-    
+describe("inicio de la aplicacion", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+  it("Incio de sesion", () => {
+    cy.get(".input-group.mt-4 > .form-control").type("a@gmail.com");
+    cy.get(".input-group.mt-1 > .form-control").type("t1");
+    cy.get(".btn").click();
+  });
+  it.only("Incio de sesion Erroneo", () => {
+    cy.get(".input-group.mt-4 > .form-control").type("a@gmail.com");
+    cy.get(".input-group.mt-1 > .form-control").type("t1");
+    cy.get(".btn").click();
+    cy.get("#asd");
+  });
 });
