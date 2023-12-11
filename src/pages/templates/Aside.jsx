1c44c709559bfd24b70a67b33ddac4959a598398
Brandon Quintero <developerbvth@gmail.com>
2023-12-11 09:31:02 -0500

    Aside

--- src/pages/templates/Aside.jsx
+++ src/pages/templates/Aside.jsx
@@ -20,7 +20,6 @@ export function Aside() {
             {/* <ProvidersAside /> */}
             <UserAside />
             <OffersAside />
-            
           </>
         )}
       </ContenedorAside>
