8d222aaed1c3f069bf13143e80bf9732fc6a78c9
Brandon Quintero <developerbvth@gmail.com>
2023-12-15 02:48:13 -0500

    cloud front

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const render = "https://rcservice.onrender.com/api";
 const localhost = "http://localhost:3000/api";
 const instance = axios.create({
-  baseURL: localhost,
+  baseURL: render,
   withCredentials: true,
 });
 export default instance;
