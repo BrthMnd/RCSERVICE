225a3b2b7a1db0b9cb9d6d69ad8e97b049c03fb8
Brandon Quintero <developerbvth@gmail.com>
2023-12-15 12:55:07 -0500

    Agregate

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: 'rc-vel.onrender.com',
-    port: 443,
-    https: true,
-  },
+  
   plugins: [react()],
 })
